a6ffad6f62fda142d7bbf6eef7880918a3494a9a
Alejandro <alejandrodiazortega@hotmail.com>
2022-07-04 17:22:27 -0500

    Explicando el uso y funcionamiento del function scope

--- src/scope/functionScope.js
+++ src/scope/functionScope.js
@@ -1,3 +1,18 @@
+/*--El function Scope es el alcance de las varibales y de toda la lógica de nuetro
+programa, es decir nuestras variables y funciones serán solo y SOLO vistas y accedidas por
+loque este dentro de la función 'padre'; Fuera de esta, no existen.--*/
+
 function greeting (){
-    
+    let userName='Alex';//Decalracion y asignacion.
+    console.log(userName);
+
+    if(userName === 'Alex'){
+        console.log(`Hello world ${userName} !`);
+    }
 }
+
+greeting();//Alex -- Hello world Alex !--
+console.log(userName);//Error.
+
+//Lo que pasa en la función, se queda en la funcion!.
+//-Proverbio chino-
